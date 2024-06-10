1f1e5595dca297ebd4d52c911a031c074b352a86
UFxx <smileprost@gmail.com>
2024-06-10 15:16:37 +0300

    Event component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/Login/Login";
 import Profile from "./components/Profile/Profile";
 import ProfileEdit from "./components/Profile/ProfileEdit/ProfileEdit";
 import Events from "./components/Events/Events";
+import Event from "./components/Events/Event/Event";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/profile-edit" element={<ProfileEdit />} />
           <Route path="/events" element={<Events />} />
+          <Route path="/event" element={<Event />} />
         </Routes>
       </BrowserRouter>
     </>
